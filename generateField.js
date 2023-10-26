90c86ff01826e9cd780f217760aaa3a5144ebbc3
Mattn4 <nky_88@hotmail.com>
2023-10-26 15:06:45 +0800

    Added comments on how to create a randomized field

--- generateField.js
+++ generateField.js
@@ -1,16 +1,22 @@
 
-// create randomized field with character starting at random location
+// Create randomized field with character starting at random location
 const generateField = (height, width, percentageOfHoles) => {
   const startingArray = ['*', '^']
   
+  // Create an array made up of holes only
+  // The number of holes is based on its percentage on the field 
   const numberOfHoles = Math.round(height*width*percentageOfHoles/100)
   const arrayOfHoles = new Array(numberOfHoles).fill('O')
   
+  // Create an array made up of fieldCharacter only
+  // Its number is number of spaces available on the field - number of holes -'*' -'^'   
   const numberofField = height*width - numberOfHoles - 2
   const arrayofField = new Array(numberofField).fill('░')
 
+  // Combine startingArray, holes array, fieldCharacter array into one single array
   const initialField = startingArray.concat(arrayOfHoles).concat(arrayofField)
 
+  // Function to shuffle the elements in an array based on Fisher-Yates Sorting Algorithm
   const shuffleArray = array => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -21,8 +27,10 @@ const generateField = (height, width, percentageOfHoles) => {
     return array
   }
 
+  // Shuffle all the elements in the initialField array
   const initialRandomizedField = shuffleArray(initialField)
 
+  // Function to convert a single array into 2D array with fixed number of elements in each sub array
   const toMatrix = (arr, width) => {
     return arr.reduce(function (rows, key, index) { 
       return (index % width == 0 ? rows.push([key]) 
@@ -30,6 +38,8 @@ const generateField = (height, width, percentageOfHoles) => {
     }, []);
   }
 
+  // Sort single array into 2D arrays
+  // Eg If width is 10, there are 10 elements in each sub array
   const finalField = toMatrix(initialRandomizedField, width)
 
   return finalField
@@ -40,6 +50,7 @@ const generateField = (height, width, percentageOfHoles) => {
 // console.log(generateField(10, 10, 10).join('\n').replace(/,/g, ''))
 
 
+//---------------- Old codes for archiving purpose ----------------------------
 /* create randomized field with character starting at top left corner
 const generateField = (height, width, percentageOfHoles) => {
 

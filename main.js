60dd89bc7c2af9b775afa1facec59e11937860f8
Mattn4 <nky_88@hotmail.com>
2023-10-29 15:54:56 +0800

    Created function for horizontal & vertical movements

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ const myField = new Field([
     ['░', '^', '░'],
   ]);
 
-//Function to start a new game or exit
+//Function to restart game or exit
 const startNewGameOrExit = () => {
   console.log('\nDo you want play again? \Enter "yes" to start a new game \nEnter anything else to exit the game')
   let question = prompt()
@@ -41,7 +41,7 @@ const startNewGameOrExit = () => {
   }
 }
 
-//------------------------------Function to Start the game--------------------------------------------
+//-------------------------------Function to Start the game--------------------------------------------
 const startGame = () => {
 
   // Generate randomized field with 3 parameters (height, width, perpercentageOfHoles)
@@ -57,6 +57,50 @@ const startGame = () => {
   }
   let v = findStartVerticalPosition() // Starting vertical position
   let h = gameGrid[findStartVerticalPosition()].indexOf('*') // Starting horizontal position
+
+  // Function for horizontal movement
+  const horizontalMovement = direction => {
+    if (direction === 'r') {
+      h++
+    } else if (direction === 'l') {
+      h--
+    }
+
+    if (h < 0 || h > gameGrid[0].length-1) {
+      console.log('Out of field! Game over')
+      startNewGameOrExit()
+    } else if (gameGrid[v][h] === hole) {
+      console.log('You trip into a hole! Game over')
+      startNewGameOrExit()
+    } else if (gameGrid[v][h] === hat) {
+      console.log('You found the hat! You win!')
+      startNewGameOrExit()
+    } else {
+      gameGrid[v][h] = pathCharacter
+    }
+  }
+
+  // Function for vertical movement
+  const verticalMovement = direction => {
+    if (direction === 'u') {
+      v--
+    } else if (direction === 'd') {
+      v++
+    }
+
+    if (v < 0 || v > gameGrid.length-1) {
+      console.log('Out of field! Game over')
+      startNewGameOrExit()
+    } else if (gameGrid[v][h] === hole) {
+      console.log('You trip into a hole! Game over')
+      startNewGameOrExit()
+    } else if (gameGrid[v][h] === hat) {
+      console.log('You found the hat! You win!')
+      startNewGameOrExit()
+    } else {
+      gameGrid[v][h] = pathCharacter
+    }
+  }
   
   console.log('\n------------------------------------------------------------------------------------')
   console.log('Welcome to the "Find Your Hat" game. \nGoal: Move your character (*) to reach the hat (^), while avoiding the holes (O). \n\nWarning: If you trip into a hole or move out of field, it is game over!')
@@ -66,85 +110,17 @@ const startGame = () => {
     console.log(gameGrid.join('\n').replace(/,/g, ''))
 
     let direction = prompt('Which way?')
-    direction = direction.toLowerCase()
-      
+    direction = direction.toLowerCase()     
+
     switch (direction) {
-      case 'r': // Moving right
-        h++
-        if (h < 0 || h > gameGrid[0].length-1) {
-          console.log('Out of field! Game over')
-          startNewGameOrExit()
-        } else if (gameGrid[v][h] === hole) {
-          console.log('You trip into a hole! Game over')
-          startNewGameOrExit()
-        } else if (gameGrid[v][h] === hat) {
-          console.log('You found the hat! You win!')
-          startNewGameOrExit()
-        } else {
-          gameGrid[v][h] = pathCharacter
-        }
-        break    
-
-        /* const grid = gameGrid[v][h]
-        switch (grid) {
-        case hole:
-            gameMessage('You trip into a hole! Game over')
-            break
-        case hat:
-            gameMessage('You found the hat! You win!')
-            break
-        default:
-            gameGrid[v][h] = pathCharacter
-            gameMessage(myField.print()) 
-            break
-        } */ 
+      case 'r': // Move right      
+      case 'l': // Move left
+        horizontalMovement(direction)
+        break   
     
-      case 'l': // Moving left 
-        h--
-        if (h < 0 || h > gameGrid[0].length-1) {
-          console.log('Out of field! Game over')
-          startNewGameOrExit()
-        } else if (gameGrid[v][h] === hole) {
-          console.log('You trip into a hole! Game over')
-          startNewGameOrExit()
-        } else if (gameGrid[v][h] === hat) {
-          console.log('You found the hat! You win!')
-          startNewGameOrExit()
-        } else {
-          gameGrid[v][h] = pathCharacter
-        }
-        break
-
-      case 'd': // Moving to down
-        v++
-        if (v < 0 || v > gameGrid.length-1) {
-          console.log('Out of field! Game over')
-          startNewGameOrExit()
-        } else if (gameGrid[v][h] === hole) {
-          console.log('You trip into a hole! Game over')
-          startNewGameOrExit()
-        } else if (gameGrid[v][h] === hat) {
-          console.log('You found the hat! You win!')
-          startNewGameOrExit()
-        } else {
-          gameGrid[v][h] = pathCharacter
-        }
-        break
-    
-      case 'u': // Moving up
-        v--
-        if (v < 0 || v > gameGrid.length-1) {
-          console.log('Out of field! Game over')
-          startNewGameOrExit()
-        } else if (gameGrid[v][h] === hole) {
-          console.log('You trip into a hole! Game over')
-          startNewGameOrExit()
-        } else if (gameGrid[v][h] === hat) {
-          console.log('You found the hat! You win!')
-          startNewGameOrExit()
-        } else {
-          gameGrid[v][h] = pathCharacter
-        }
+      case 'u': // Move up
+      case 'd': // Move down
+        verticalMovement(direction)
         break
 
       case 'q': // quit the game
@@ -160,28 +136,26 @@ const startGame = () => {
 
 }
 
+// Start the game
 startGame()
 
-/* function gameMessage(result = ""){
+
+/*----------------------------------------------------------------------------------------------------
+function gameMessage(result = ""){
     console.log(result);
 } 
-retain this gameMessage function, but use back original if else statements instead of switch */
+retain this gameMessage function, but use back original if else statements instead of switch 
 
 
-/* Main issues 
+Main issues 
 gameGrid[v][h]
   if h is out of range, result be undefined
   but if v is out of range, will appear error
   so cannot use "gameGrid[v][h] === undefined"
-*/
 
-/*
+
 Enhancements to considers
 Change all console log (print message) to single function (game message function)
 in the game message function:
 If win the game, ask player if wanna play another round or at harder level?
-If lose, wanna play the game again or quit? (Game stickiness)
-
-
-consolidate vertical & horizontal condition into separate functions 
 */
